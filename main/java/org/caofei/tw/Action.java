78d8076548c27ba4249d66e36904371cef13cdf0
cfly <caofei.org@gmail.com>
2014-06-09 18:03:14 +0800

    uml

---
+++ main/java/org/caofei/tw/Action.java
@@ -0,0 +1,8 @@
+package org.caofei.tw;
+
+/**
+ * Action
+ * Created by caof on 2014/6/9.
+ */
+public interface Action {
+}
---
+++ main/java/org/caofei/tw/Left.java
@@ -0,0 +1,8 @@
+package org.caofei.tw;
+
+/**
+ * Left impl
+ * Created by caof on 2014/6/9.
+ */
+public class Left implements Action {
+}
